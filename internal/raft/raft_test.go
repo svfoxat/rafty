6f3512df463acbbd1a7eb7e1cb5e9ac1f7e9c926
Nico Nössler <nico@streamstack.tv>
2025-03-15 15:52:06 +0100

    feat: added testhelpers

--- internal/raft/raft_test.go
+++ internal/raft/raft_test.go
@@ -1,87 +1,74 @@
-package raft_test
+package raft_test //nolint:typecheck
 
 import (
 	"context"
+	"fmt"
 	"github.com/svfoxat/rafty/internal/raft"
 	"testing"
 	"time"
 )
 
-func getLeaderID(nodes ...*raft.Raft) int32 {
-	for _, node := range nodes {
-		if node.GetState() == raft.Leader {
-			return node.ID
-		}
-	}
-	return -1
-}
-
 func TestLeaderElection(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	// Define addresses for three nodes.
-	node1Addr := "127.0.0.1:12345"
-	node2Addr := "127.0.0.1:12346"
-	node3Addr := "127.0.0.1:12347"
+	start := time.Now()
+	nodes := StartCluster(ctx, t, 3)
+	leader, err := CheckLeader(ctx, nodes)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	// Create three Raft nodes with unique IDs and their peer addresses.
-	node1 := raft.NewNode(1, []string{node2Addr, node3Addr})
-	node2 := raft.NewNode(2, []string{node1Addr, node3Addr})
-	node3 := raft.NewNode(3, []string{node1Addr, node2Addr})
+	t.Logf("leader is %+v after %dms", leader, time.Since(start).Milliseconds())
+}
 
-	go func() {
-		err := node1.Start(ctx, "127.0.0.1", 12345)
-		if err != nil {
-			t.Errorf("failed to start node1: %v", err)
-			return
-		}
-	}()
+func StartCluster(ctx context.Context, t *testing.T, count int) []*raft.Raft {
+	var nodes []*raft.Raft
+	var peers []string
 
-	go func() {
-		err := node2.Start(ctx, "127.0.0.1", 12346)
-		if err != nil {
-			t.Errorf("failed to start node3: %v", err)
-			return
-		}
-	}()
+	for i := 0; i < count; i++ {
+		peers = append(peers, fmt.Sprintf("127.0.0.1:%d", 12345+i))
+	}
+	for i := 1; i <= count; i++ {
+		nodes = append(nodes, raft.NewNode(int32(i), peers))
+	}
 
-	go func() {
-		err := node3.Start(ctx, "127.0.0.1", 12347)
-		if err != nil {
-			t.Errorf("failed to start node3: %v", err)
-			return
-		}
-	}()
+	// start the nodes
+	for idx, node := range nodes {
+		go func() {
+			node.Start(ctx, "127.0.0.1", 12345+idx)
+		}()
+	}
+	return nodes
+}
 
-	// Poll the nodes for a leader for up to 10 seconds.
+// CheckLeader Poll the nodes for a leader for up to 10 seconds.
+func CheckLeader(ctx context.Context, nodes []*raft.Raft) (int, error) {
 	timeout := time.After(10 * time.Second)
 	ticker := time.NewTicker(10 * time.Millisecond)
 	defer ticker.Stop()
 
-	start := time.Now()
 	for {
 		select {
+		case <-ctx.Done():
+			return -1, ctx.Err()
 		case <-timeout:
-			t.Fatal("Timeout: no leader elected after 10 seconds")
+			return -1, fmt.Errorf("timeout: no leader elected after 10 seconds")
 		case <-ticker.C:
 			leaderCount := 0
-			if node1.GetState() == raft.Leader {
-				leaderCount++
-			}
-			if node2.GetState() == raft.Leader {
-				leaderCount++
-			}
-			if node3.GetState() == raft.Leader {
-				leaderCount++
+			var leaders []int
+
+			for _, node := range nodes {
+				if node.GetState() == raft.Leader {
+					leaderCount++
+					leaders = append(leaders, int(node.ID))
+				}
 			}
 			if leaderCount > 1 {
-				t.Fatalf("Error: more than one leader elected; found %d leaders", leaderCount)
+				return -1, fmt.Errorf("more than one leader elected; found %d leaders", leaderCount)
 			}
 			if leaderCount == 1 {
-				leaderID := getLeaderID(node1, node2, node3)
-				t.Logf("Leader elected after %dms: Node %d", time.Since(start).Milliseconds(), leaderID)
-				return
+				return leaders[0], nil
 			}
 		}
 	}
